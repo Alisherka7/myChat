a7081766f35b1b6d2707d537f0d60829d8ffa539
alisherka7 <tursunov7772@gmail.ru>
2022-01-28 01:36:20 +0900

    commit message

---
+++ myChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myChat
+//
+//  Created by alisherka7 on 2022/01/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
